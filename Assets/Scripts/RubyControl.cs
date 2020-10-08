a73b1dbd88610bb9a83f587fc2c9f0fb62f32943
Evy Avila <384036@colum.edu>
2020-10-08 04:02:44 -0500

    Added decoration to Ruby worlds

---
+++ Assets/Scripts/RubyControl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Security.Cryptography;
+using System.Threading;
+using UnityEngine;
+
+public class RubyControl : MonoBehaviour
+{
+
+    private void Update()
+    { 
+        float horizontal = Input.GetAxis("Horizontal");
+        float veritcal = Input.GetAxis("Vertical");
+        Vector2 position = transform.position;
+        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
+        position.y = position.y + 3.0f * veritcal * Time.deltaTime;
+        transform.position = position;
+    }
+
+}
